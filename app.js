314fe5dbd8e937591ff3e62f0de4982ba7aee24e
Jorge Romero <jorge.romeropalma@gmail.com>
2020-09-27 19:25:59 +0200

    Print some env variables

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-	  res.send('Hello World!');
+      body = "Hello world! \nvariable:MY_VARIABLE: " + process.env.MY_VARIABLE  + "\nMY_SECRET: " + process.env.MY_SECRET
+	  res.send(body);
 });
 
 app.listen(3000, function () {
