4e90917d66f61eb1e513e4ece289145d5ee6eb1d
Jorge Romero <jorge.romeropalma@gmail.com>
2020-09-27 19:43:21 +0200

    Some config and liveness and readiness probes

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get('/api/liveness', function(req, res) {
+        res.send('Alive');
+});
+
+
+app.get('/api/readiness', function(req, res) {
+        res.send('Ready');
+});
+
 
 app.get('/', function (req, res) {
       body = "Hello world! \nvariable:MY_VARIABLE: " + process.env.MY_VARIABLE  + "\nMY_SECRET: " + process.env.MY_SECRET
 	  res.send(body);
 });
 
-app.listen(8080, function () {
-	  console.log('Example app listening on port 3000!');
+app.listen(PORT, function () {
+	  console.log('Example app listening on port '+PORT+'!');
 });
 
